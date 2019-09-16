180d1a9e4d907d495fac6b5b691fdab6569f6b8b
Kira Roberts <rosehill1788@protonmail.com>
2019-09-16 11:25:10 -0700

    add initial file structure and webpack build

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { Game } from './alienatingRPG';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+$(document).ready(function() {
+
+});
