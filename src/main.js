1c7e1f0cde30ad13a9b102a565956cd2525a82c2
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-18 16:46:41 -0700

    start js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 
-$(document).ready(function() {
 
+$(document).ready(function() {
+  $("#character-card1").show();
+  $("#character-card2").hide();
+  $("#character-card3").hide();
+  $("#character-card4").hide();
+
+  $("#swipe-left").click(function(event) {
+    $("#character-card1").hide();
+    $("character-card2").show();
+    if
+
+
+
+  });
+  // $("#hidden_content").slideToggle("slow");
 });
